688706a8519bad2a3e8ae2c3234f58bd5d399e66
Shen Cong <congkevin@gmail.com>
2017-08-26 22:39:51 -0400

    remove error lines

--- conf.py
+++ conf.py
@@ -181,7 +181,3 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
-
-MathJax.Hub.Config({
-    displayAlign: "left"
-});
